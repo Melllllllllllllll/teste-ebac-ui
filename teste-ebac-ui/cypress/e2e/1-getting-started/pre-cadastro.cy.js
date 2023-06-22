0b0e38d0d8cc2e8ad290ff667b2ec26ad0e7682a
Mellllllllllllll <melissa.qualideade@gmail.com>
2023-06-22 12:13:21 -0300

    fix:Criação e alteração de variaves nos testes: pre-cadastro e produtos

--- teste-ebac-ui/cypress/e2e/1-getting-started/pre-cadastro.cy.js
+++ teste-ebac-ui/cypress/e2e/1-getting-started/pre-cadastro.cy.js
@@ -8,13 +8,19 @@ describe('Funcionalidade Pré cadastro com sucesso', () => {
     });
 
     it('Deve completar o pré cadastro com sucesso', () => {
-        cy.get('#reg_email').type(faker.internet.email())
+       let nomeFaker = faker.name.firstName()
+       let sobrenomeFaker = faker.name.fullName()
+       let emailFaker = faker.internet.email(nomeFaker)
+
+
+
+        cy.get('#reg_email').type(emailFaker)
         cy.get('#reg_password').type('marddif3ff236g5d45910')
         cy.get(':nth-child(4) > .button').click()
 
         cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
-        cy.get('#account_first_name').type(faker.name.firstName())
-        cy.get('#account_last_name').type(faker.name.fullName())
+        cy.get('#account_first_name').type(nomeFaker)
+        cy.get('#account_last_name').type(sobrenomeFaker)
         cy.get('.woocommerce-Button').click()
 
         cy.get('.woocommerce-message').should('contain' , 'Detalhes da conta modificados com sucesso.')
